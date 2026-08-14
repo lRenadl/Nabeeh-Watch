ba7dba5cfe2a92a52418f50a7fee918d66760050
Renad Alowais <renadalowais@alMacBook-Air-alkhas-bRenad.local>
2026-08-14 22:21:19 +0300

    Initial commit - watch firmware project

---
+++ include/ui_fonts.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <lvgl.h>
+
+LV_FONT_DECLARE(tajawal_bold_24)
+LV_FONT_DECLARE(tajawal_regular_16)
---
+++ include/ui_images.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <lvgl.h>
+
+extern const lv_image_dsc_t nabeeh_logo;
+extern const lv_image_dsc_t emoji_bell;
+extern const lv_image_dsc_t emoji_knock;
+extern const lv_image_dsc_t emoji_baby;
+extern const lv_image_dsc_t emoji_fire;
+extern const lv_image_dsc_t emoji_mosque;
+
+extern const lv_image_dsc_t sign_doorbell;
+extern const lv_image_dsc_t sign_knock;
+extern const lv_image_dsc_t sign_baby;
+extern const lv_image_dsc_t sign_fire;
+extern const lv_image_dsc_t sign_adhan;
